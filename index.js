4b72ba4ee6fec0852c525e9674bc4827cf563f5d
Ojosh <111340682+ojoscreate@users.noreply.github.com>
2026-09-21 12:48:01 +0100

    updated nav

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+// update the year in the footer
 document.getElementById("yr").textContent = new Date().getFullYear();
+
+// navbar
+const navToggle = document.querySelector(".hamburger");
+const navmenu = document.querySelectorAll(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navmenu.forEach((menu) => {
+      menu.classList.remove("active");
+    });
+  });
+});
+
+navToggle.addEventListener("click", () => {
+  navmenu.forEach((menu) => {
+    menu.classList.toggle("active");
+  });
+});
